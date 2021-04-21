ca6fb52e895ab0ce6cde3c10ece8ed1763d2abc2
zeshan13 <38884447+zeshan13@users.noreply.github.com>
2021-04-21 08:52:41 +0800

    Update logger.py

--- selenium_demo/comm/logger.py
+++ selenium_demo/comm/logger.py
@@ -3,3 +3,34 @@
 # @Time    : 2021-04-21 0:02
 # @Author  : zeshan
 # @File    : logger.py
+import logging
+import time
+import config
+cfg = config.COMMCFG
+
+class Logger(object):
+
+    def __init__(self,logger):
+        self.logger = logging.getLogger(logger)
+        self.logger.setLevel(logging.DEBUG)
+
+        rq = time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
+        log_name = cfg.logs_dir + rq + '.log'
+        fh = logging.FileHandler(log_name)
+        fh.setLevel(logging.INFO)
+
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+
+        self.logger.addHandler(fh)
+        self.logger.addHandler(ch)
+
+    def getLog(self):
+        return self.logger
+
+if __name__ == '__main__':
+    print(cfg.logs_dir)
